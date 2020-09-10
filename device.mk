cc7f5a41caeab8dcf194913845e5352187d124bc
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    tp1803: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- device.mk
+++ device.mk
@@ -171,8 +171,10 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-   android.hidl.base@1.0 \
-   android.hidl.base@1.0_system
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 
 # Init
 PRODUCT_PACKAGES += \
