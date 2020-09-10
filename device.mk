a5772dfb058975775eb83bd3b1464cbf9a9f15ec
Anay Wadhera <awadhera@berkeley.edu>
2020-09-10 00:59:31 -0700

    tp1803: add custom lights HAL

--- device.mk
+++ device.mk
@@ -222,8 +222,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service \
-    android.hardware.light@2.0-impl
+    android.hardware.light@2.0-service.nubia_tp1803 \
 
 # Lockscreen
 PRODUCT_COPY_FILES += \
