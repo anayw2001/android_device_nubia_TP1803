762237f949b3538418fe1a4e8928d2dc85059802
Anay Wadhera <awadhera@berkeley.edu>
2020-09-17 01:11:45 -0700

    Revert "move to lineage extract utils"
    This reverts commit 2c5a518581711182800dcf9bb4911d5f30bc4da4.

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=nubia
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+STATIX_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${STATIX_ROOT}/vendor/statix/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -60,10 +60,10 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${STATIX_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
-sed -i "s/libc++.so/libcXD.so/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/hw/camera.qcom.so
+sed -i "s/libc++.so/libcXD.so/g" "$STATIX_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/hw/camera.qcom.so
 
 "${MY_DIR}/setup-makefiles.sh"
