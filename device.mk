27955892424080e953e906326120bf4c6a62cc52
Anay Wadhera <awadhera@berkeley.edu>
2020-09-28 11:30:24 -0700

    tp1803: add lights HAL kanged from OnePlus sdm845-common
    * adapted for TP1803

--- device.mk
+++ device.mk
@@ -199,6 +199,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.tp1803
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
