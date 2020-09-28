a72f89e975cdedcb5b7069d636377c3615b5e444
Anay Wadhera <awadhera@berkeley.edu>
2020-09-28 13:31:13 -0700

    tp1803: move recovery init to recovery system from root

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,12 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
