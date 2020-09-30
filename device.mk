69f516f856b56d55b65421c3a90eeb38d84b8744
Jake Weinstein <jake@aospa.co>
2020-10-01 00:47:45 +0200

    mini5g: remove seccomp
    We have this in vendor/pa

--- device.mk
+++ device.mk
@@ -254,11 +254,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-service \
