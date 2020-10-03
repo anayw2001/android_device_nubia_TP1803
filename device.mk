5c74c0b8754df7c4c76f71524887dd9cc6089167
Anay Wadhera <awadhera@berkeley.edu>
2020-10-03 09:43:24 -0700

    build btconfigstore from source

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbt-vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
