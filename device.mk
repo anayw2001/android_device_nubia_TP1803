0d1a0082828d75e29f10ae8b15ab48d3612b98cf
Anay Wadhera <awadhera@berkeley.edu>
2020-10-13 14:40:54 -0700

    tp1803: enable blur

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Blur
+TARGET_USES_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.device@3.4-impl \
