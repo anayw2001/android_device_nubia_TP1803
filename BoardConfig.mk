0077a2f5fe1afae3a255d76b9e54ba771613ce3e
Anay Wadhera <awadhera@berkeley.edu>
2020-10-21 22:43:59 -0700

    tp1803: create vintf data

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,8 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Kernel
